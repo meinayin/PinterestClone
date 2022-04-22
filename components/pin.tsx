da928f335c576f3c15586709dcb9fa1bfe9d11c3
norma <normayin1104@hotmail.com>
2022-04-22 13:16:42 -0700

    Home Screen Masonry Layout

--- components/pin.tsx
+++ components/pin.tsx
@@ -27,11 +27,11 @@ const Pin = (props) => {
               style={[styles.image, { aspectRatio: ratio }]}
             />
             <Pressable onPress={onLike} style={styles.heartBtn}>
-                <AntDesign name='hearto' size={18} color="black" />
+                <AntDesign name='hearto' size={16} color="black" />
             </Pressable>
           </View>
           
-        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.title} numberOfLines={2}>{title}</Text>
       </View>
     )
 }
@@ -39,15 +39,18 @@ const Pin = (props) => {
 const styles = StyleSheet.create({
     pin: {
         width: '100%',
+        padding: 4,
       },
       title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        margin: 10,
+        fontSize: 15,
+        lineHeight:22,
+        fontWeight: '600',
+        margin: 5,
+        color: '#181818',
       },
       image: {
         width: '100%',
-        borderRadius: 25,
+        borderRadius: 15,
       },
       heartBtn:{
         backgroundColor: '#d3cfd4',
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,21 +2,26 @@ import { StyleSheet, Image, ScrollView } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import Pin from '../components/pin';
+import pins from '../assets/data/pins';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <ScrollView>
          <View style={styles.container}>
-            <Pin pin={{
-              title: "Title",
-              image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/2.jpeg"
-              }}
-            />
-            <Pin pin={{
-              title: "2nd Title",
-              image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/3.jpeg"
-              }}
-            />
+           <View style={styles.column}>
+              {pins
+                .filter((item, index) => index % 2 === 0)
+                .map((pin => <Pin pin={pin} key={pin.id} />
+                ))}
+
+           </View>
+
+           <View style={styles.column}>
+              {pins
+                .filter((item, index) => index % 2 === 1)
+                .map((pin => <Pin pin={pin} key={pin.id} />
+                ))}
+           </View>
         </View>
     </ScrollView>
   );
@@ -24,9 +29,10 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>)
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: 10,
+    flexDirection: 'row',//split screen into two columns
   },
+  column: {
+    flex: 1,
+  }
 });
