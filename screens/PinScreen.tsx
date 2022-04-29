ee98eb5244b1aa7b290a1ccbb14ef2a2115f6325
norma <normayin1104@hotmail.com>
2022-04-29 11:18:43 -0700

    query pins from hasura qraphql

--- screens/PinScreen.tsx
+++ screens/PinScreen.tsx
@@ -4,17 +4,45 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import pins from '../assets/data/pins'
+import { useNhostClient } from "@nhost/react";
+import { Alert } from 'react-native';
+
+const GET_PIN_QUERY = `
+    query MyQuery ($id: uuid!) {
+        pins_by_pk(id: $id) {
+            created_at
+            id
+            image
+            title
+            user_id
+            user {
+                avatarUrl
+                displayName
+            }
+        }
+    }
+`
 
 const PinScreen = () => {
     const [ratio, setRatio] = useState(1);
+    const [pin, setPin] = useState([]);
+
+    const nhost = useNhostClient();
+
     const insets = useSafeAreaInsets();
     const navigation = useNavigation();
     const route = useRoute();
 
     const pinId = route.params?.id;
 
-    const pin = pins.find(p => p.id === pinId);
+    const fetchPin = async (pinId) => {
+        const response = await nhost.graphql.request(GET_PIN_QUERY, { id: pinId });
+        if(response.error){
+            Alert.alert("Error fetching the pin")
+        }else{
+            setPin(response.data.pins_by_pk);
+        }
+    };
 
     const goBack = () => {
         navigation.goBack();
@@ -25,7 +53,11 @@ const PinScreen = () => {
     }
 
     useEffect(() => {
-        if(pin.image){
+        fetchPin(pinId);
+    }, [pinId]);
+
+    useEffect(() => {
+        if(pin?.image){
           Image.getSize(pin.image, (width, height) => setRatio(width / height));
         }
       },[pin]);//set ratio as image changes
