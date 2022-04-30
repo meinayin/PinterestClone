79740aa04a74fbf6c372e8c6a1c778dc0db011d4
norma <normayin1104@hotmail.com>
2022-04-29 22:28:17 -0700

    Improvments and finish touches

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,13 +1,52 @@
 import { Entypo, Feather } from '@expo/vector-icons';
-import { useSignOut } from '@nhost/react';
-import { StyleSheet, Image, ScrollView, Pressable } from 'react-native';
+import { NhostReactProvider, useNhostClient, useSignOut, useUserId } from '@nhost/react';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Image, ScrollView, Pressable, Alert, ActivityIndicator } from 'react-native';
 import pins from '../assets/data/pins';
 import MasonryList from '../components/MasonryList';
 import { Text, View } from '../components/Themed';
 
-export default function ProfileScreen() {
+const GET_USER_QUERY = `
+  query MyQuery($id: uuid!){
+    user(id: $id){
+      id
+      avatarUrl
+      displayName
+      pins{
+        id
+        image
+        title
+        created_at
+      }
+    }
+  }
+`;
 
-    const {signOut} = useSignOut();
+export default function ProfileScreen() {
+  const [user, setUser] = useState();
+
+    const { signOut } = useSignOut();
+
+    const userId = useUserId();
+    const nhost = useNhostClient();
+
+    const fetchUserData = async () => {
+      const result = await nhost.graphql.request(GET_USER_QUERY, { id: userId });
+      if(result.error){
+        Alert.alert("Error fetching the user");
+      }else{
+        setUser(result.data.user);
+      }
+    };
+
+    useEffect(() => {
+      fetchUserData();
+    }, []);
+
+    if(!user){
+      return <ActivityIndicator />
+    }
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.header}>
@@ -23,14 +62,14 @@ export default function ProfileScreen() {
             />
         </View>
           <Image source={{
-              uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png",
+              uri: user.avatarUrl,
           }}
           style={styles.image}
           />
         <Text style={styles.title}>Vadim</Text>
         <Text style={styles.subtitle}>123 Followers | 345 Followings</Text>
       </View>
-      <MasonryList pins={pins} />
+      <MasonryList pins={user.pins} />
     </ScrollView>
   );
 }
